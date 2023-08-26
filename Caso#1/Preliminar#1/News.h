cc469b3dd78921d03d7bf198f8dd4a3930faea0d
Miguel Aguero Mata <miguel.eduardo17197@gmail.com>
2023-08-25 20:58:48 -0600

    Mas metodos a News.h

--- Caso#1/Preliminar#1/News.h
+++ Caso#1/Preliminar#1/News.h
@@ -5,15 +5,17 @@ void getTopFive()
 {
     //Muestra las primeras cinco noticias que se encuentren en la lista.
     //printTopFive();
+    //findPosition();
 }
 
 void getAll()
 {
     //Muestra toda la lista.
     //printList();
+    //findPosition();
 }
 
-void relocate()
+void relocate(int/*si vamos a usar posicion para seleccionar*/ string/*si vamos a usar una palabra o nombre especifico*/ dataAMover, int cuantoSeQuiereMover)
 {
     //Mover una noticia a otra posicion en la lista.
     //      find(string pNoticia) si quiere mover un titulo especifico
@@ -31,4 +33,16 @@ void deleteByName(string newsName)
 {
     //Borra el la noticia del nombre indicado
     //   deleteByName();
+}
+
+void addHeadline()
+{
+    //Agregar datos a la lista
+    //  add(string pData);
+}
+
+void addHeadlineInPosition(int position);
+{
+    //Agrega un headline en una posicion especifica
+    //   insert(string pData, int position);
 }
