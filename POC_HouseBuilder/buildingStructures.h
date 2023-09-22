06c51a67d6f5478539edcaeff1a8965db18abc6b
Miguel Aguero Mata <miguel.eduardo17197@gmail.com>
2023-09-21 23:44:43 -0600

    buildingStrctures

---
+++ POC_HouseBuilder/buildingStructures.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Wall
+{
+    public:
+        int wallLength;
+        int wallHeight;
+
+    private:
+
+};
+
+class DoorWall
+{
+    public:
+        int wallLength;
+        int wallHeight;
+
+        int doorHeight;
+        int doorLength;
+        string doorMaterial;
+
+    private:
+
+};
