a725a7585bbb6b1e5ed24f6c84601a40945f543b
Miguel Aguero Mata <miguel.eduardo17197@gmail.com>
2023-09-22 02:04:30 -0600

    Update Structures

--- POC_HouseBuilder/buildingStructures.h
+++ POC_HouseBuilder/buildingStructures.h
@@ -1,3 +1,4 @@
+#ifndef BUILDINGSTRUCTURES
 #include <iostream>
 #include <string>
 
@@ -6,24 +7,90 @@ using namespace std;
 
 class Wall
 {
-    public:
+    private:
         int wallLength;
         int wallHeight;
+        string wallMaterial;
+        string wallColor;
 
-    private:
+    public:
+        void getWallSpecifications(int wLength, int wHeight, string wMaterial, string wColor)
+        {
+            wallLength = wLength;
+            wallHeight = wHeight;
+            wallMaterial = wMaterial;
+            wallColor = wColor;
+        }
 
 };
 
 class DoorWall
 {
-    public:
+    private:
         int wallLength;
         int wallHeight;
+        string wallMaterial;
+        string wallColor;
 
         int doorHeight;
         int doorLength;
         string doorMaterial;
 
-    private:
+     public:
+        void getWallDoorSpecifications(int wLength, int wHeight, string wMaterial, string wColor, int dHeight, int dLength, string dMaterial)
+        {
+            wallLength = wLength;
+            wallHeight = wHeight;
+            wallMaterial = wMaterial;
+            wallColor = wColor;
+            doorHeight = dHeight;
+            doorLength = dLength;
+            doorMaterial = dMaterial;
+        }
 
 };
+
+class Balcony
+{
+    private:
+        int balconyLength;
+        int balconyWidth;
+
+     public:
+        void getBalconySpecifications(int bLength, int bWidth)
+        {
+            balconyLength = bLength;
+            balconyWidth = bWidth;
+        }
+};
+
+class Floor
+{
+    private:
+        string floorMaterial;
+        string floorColor;
+
+    public:
+        void getFloorSpecifications(string fMaterial, string fColor)
+        {
+            floorMaterial = fMaterial;
+            floorColor = fColor;
+        }
+    
+};
+
+class Stairs
+{
+    private:
+        string stairsColors;
+        string stairsMaterial;
+
+    public:
+        void getStarisSpecifications(string sColors, string sMaterial)
+        {
+            stairsColors = sColors;
+            stairsMaterial = sMaterial;
+        }
+    
+};
+#endif
